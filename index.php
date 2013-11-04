8f37c947f84359b7ada4c319163f13161869adee
Zachary Towell <zttowell@gmail.com>
2013-11-03 18:18:21 -0600

    Added UL for nav bar

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
-<h1>
-Under Construction
-</h1>
+	<ul>
+		<li>Home</li>
+		<li>About</li>
+		<li>Contact</li>
+		<li>Churches</li>
+		<li>Calendar</li>
+		<li>Employees</li>
+		<li>Missons</li>
+		<li>Reaching Out</li>
+		<li>Staff Positions</li>
+		<li>Counseling</li>
+		<li>Resources</li>
+	</ul>
+
+
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 </html>
